f7080d09f13760b66ec8ec57e8756a8a6e822f2d
akashverma <akashv@banquemisr.com>
2024-07-27 17:27:07 +0530

    comment added

--- src/main/kotlin/com/akash/myjdbc/MyjdbcApplication.kt
+++ src/main/kotlin/com/akash/myjdbc/MyjdbcApplication.kt
@@ -10,6 +10,8 @@ class MyjdbcApplication
 
 fun main(args: Array<String>) {
     runApplication<MyjdbcApplication>(*args)
+
+    // code to demonstrate creation of 'Custom' spring beans see "MyBeans.kt"
     val context = AnnotationConfigApplicationContext(MyBeans::class.java)
     val name = context.getBean("name")
     val customBean = context.getBean("customBean")
